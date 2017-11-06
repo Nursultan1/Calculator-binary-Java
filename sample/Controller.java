2438108478038d8fe15278283946650f27e84512
Askarbekov Nursultan <nursultanabdazov704@gmail.com>
2017-11-06 15:44:44 +0600

    Вкладки

--- sample/Controller.java
+++ sample/Controller.java
@@ -1,18 +1,31 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
-public class Controller {
+public class Controller  {
+
     public TextField textField;
     public double a;
     public double b;
-    public char oper;
     public String state="0";
+
+    public TextField textField2;
+    public String aBin;
+    public String bBin;
+    public String stateBin="0";
+
     public void multiplition(double i, double j){
         double res=i*j;
         textField.setText(String.valueOf(res));
@@ -70,6 +83,85 @@ public class Controller {
     }
 
     public void clear(ActionEvent actionEvent) {
+
         textField.setText("");
+    }
+
+    public void portal(ActionEvent actionEvent) {
+        System.out.print("fvfdvf");
+    }
+
+
+//    public void portal(ActionEvent actionEvent) throws IOException {
+//        Stage primaryStage = new Stage();
+//        Parent root;
+//        root = FXMLLoader.load(getClass().getResource("binary.fxml"));
+//        Scene normal=new Scene(root, 300, 275);
+//        primaryStage.setScene(normal);
+//        primaryStage.show();
+//
+//    }
+
+    //Binary Calculator
+
+    public void numbe(ActionEvent actionEvent){
+        System.out.println(actionEvent.getTarget());
+        char str[]=actionEvent.getTarget().toString().toCharArray();
+        System.out.println(str[str.length-2]);
+        if(stateBin=="0"){
+            textField2.setText(textField2.getText()+Character.toString(str[str.length-2]));
+        }
+        else{
+            textField2.setText(textField2.getText()+Character.toString(str[str.length-2]));
+        }
+    }
+
+
+    public void opertionBinary(ActionEvent actionEvent){
+        aBin=textField2.getText();
+        textField2.setText("");
+        System.out.println(actionEvent.getTarget());
+        char str[]=actionEvent.getTarget().toString().toCharArray();
+        state=Character.toString(str[str.length-2]);
+
+    }
+
+    public void resultBin(ActionEvent actionEvent) {
+        String resul="";
+        bBin=textField2.getText();
+        switch (stateBin){
+            case "*" :
+                char aArray[]=aBin.toCharArray();
+                char bArray[]=bBin.toCharArray();
+                int len;
+                if(aArray.length<bArray.length){
+                    len=bArray.length-1;
+                }
+                else if(aArray.length>bArray.length){
+                    len=aArray.length-1;
+                }
+                else{
+                    len=aArray.length-1;
+                }
+                for(int i=0; i<len; i++ ){
+                    char k=aArray[aArray.length-i];
+                    char l=aArray[aArray.length-i];
+                    if(k==0 && l==0 ){
+
+                    }
+                }
+
+                break;
+            case "/" :
+                dele(a,b);
+                break;
+            case "+" :
+                plus(a,b);
+                break;
+            case "-" :
+                minus(a,b);
+                break;
+
+        }
     }
 }
--- sample/Main.java
+++ sample/Main.java
@@ -6,18 +6,24 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
+
+    public static void main(String[] args) {
+
+    launch(args);
+
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Calculator");
+        Scene normal= new Scene(root);
+        primaryStage.setScene(normal);
         primaryStage.show();
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+
 }
